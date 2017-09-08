beabcf6501231b6ae97c8c9322dbdeb03071da59
phankunanon <phankunanon@gmail.com>
2017-09-08 17:19:30 +0700

    add gold

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
 import arcade.key
+class Model:
+    def __init__(self,world,x,y,angle):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.angle = 0
 
-class Ship:
+class Ship(Model):
     DIR_HORIZONTAL = 0
     DIR_VERTICAL = 1
  
     def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
+        super().__init__(world, x, y, 0)
         self.direction = Ship.DIR_VERTICAL
-        self.angle = 0 
  
     def switch_direction(self):
         if self.direction == Ship.DIR_HORIZONTAL:
@@ -29,11 +32,15 @@ class Ship:
             if self.x > self.world.width:
                 self.x = 0
             self.x += 5
+class Gold(Model):
+    def __init__(self,world,x,y):
+        super().__init__(world, x, y, 0)
 
 class World:
     def __init__ (self,width,height):
         self.width = width
         self.height = height
+        self.gold = Gold(self,400,400)
 
         self.ship = Ship(self,100,100)
     def on_key_press(self,key,key_modifiers):
--- space.py
+++ space.py
@@ -9,7 +9,7 @@ class ModelSprite(arcade.Sprite):
         self.model = kwargs.pop('model',None)
 
         super().__init__(*args,**kwargs)
-    
+
     def sync_with_model(self):
         if self.model:
             self.set_position(self.model.x,self.model.y)
@@ -26,7 +26,7 @@ class SpaceGameWindow (arcade.Window):
 
         self.world = World(width,height)
         self.ship_sprite =  ModelSprite('images/ship.png',model=self.world.ship)
-        
+        self.gold_sprite = ModelSprite('images/Gold.png',model=self.world.gold)
 
     def update(self,delta):
         self.world.update(delta)
@@ -35,6 +35,7 @@ class SpaceGameWindow (arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.ship_sprite.draw()
+        self.gold_sprite.draw()
 
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
