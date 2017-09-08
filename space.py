a304f62f150ca1d2addca75ee4c8b446800b8e9c
phankunanon <phankunanon@gmail.com>
2017-09-08 15:55:26 +0700

    space moving in y with 2 code and have class world

--- space.py
+++ space.py
@@ -1,22 +1,22 @@
 import arcade
 
-from models import Ship
+from models import World,Ship
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 class SpaceGameWindow (arcade.Window):
     def __init__(self,width,height):
         super().__init__(width,height)
-        
+
         arcade.set_background_color(arcade.color.BLACK)
         
         self.ship_sprite = arcade.Sprite("images/ship.png")
-        self.ship = Ship(100,100)
+        
+        self.world = World(width,height)
 
     def update(self,delta):
-        ship = self.ship        
-        ship.update(delta)
-        self.ship_sprite.set_position(ship.x,ship.y)
+        self.world.update(delta)
+        self.ship_sprite.set_position(self.world.ship.x,self.world.ship.y)
     
     def on_draw(self):
         arcade.start_render()
